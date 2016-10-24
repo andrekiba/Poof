3aab0323d799cdf369064a4763ac1355d1b76eb5
Andrea <andre.kiba@gmail.com>
2016-10-24 14:14:10 +0200

    update

---
+++ Droid/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,16 @@
+using Poof.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace Poof.Droid.Renderers
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
